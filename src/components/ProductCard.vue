ee16183d77df70e10513f2e0c8cbde79a2b30b7b
Nelson <xhyskoax@gmail.com>
2026-03-11 21:50:58 -0300

    se crea componente ProductCard y se actualiza vista home con una grilla de productos, considerando también el deploy de la app en firebase hosting

--- src/components/ProductCard.vue
+++ src/components/ProductCard.vue
@@ -1,13 +1,31 @@
 <template>
     <div>
-
+        <div class="card">
+            <img :src="producto.imagen" class="card-img-top" :alt="producto.nombre">
+            <div class="card-body">
+                <h5 class="card-title">{{ producto.nombre }}</h5>
+                <p class="card-text">{{ producto.descripcion }}</p>
+                <p class="card-text">Precio: {{ producto.precio }}</p>
+                <p class="card-text">Stock: {{ producto.stock }}</p>
+                <p class="card-text">Categoría: {{ producto.categoria }}</p>
+                <a href="#" class="btn btn-primary">Ver más</a>
+            </div>
+        </div>
     </div>
 </template>
 
 <script setup>
 
+defineProps(["producto"]);
+
 </script>
 
 <style scoped>
+.card-img-top {
+    aspect-ratio: 3/2;
+}
 
+.card-body {
+    min-height: 330px;
+}
 </style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,14 +1,40 @@
 <template>
 
-    <HeaderComp>Página Home</HeaderComp>
-  <main class="container">
+  <HeaderComp>Página Home</HeaderComp>
+
+  <main class="container py-3">
+
+    <section>
+      <h2 class="text-center">Nuestros productos...</h2>
+
+
+      <div class="row justify-content-start g-3 row-cols-1 row-cols-sm-2 row-cols-md-3 row-cols-lg-4" v-if="productStore.products.length > 0">
+        <div class="col" v-for="producto in productStore.products">
+          <ProductCard :producto />
+        </div>
+      </div>
+
+      <div v-else>
+        <p>Leyendo productos...</p>
+      </div>
+
+    </section>
 
   </main>
 </template>
 
 <script setup>
 import HeaderComp from '@/components/HeaderComp.vue';
+import ProductCard from '@/components/ProductCard.vue';
+import { useProductsStore } from '@/stores/products.store';
+import { onMounted } from 'vue'
 
+const productStore = useProductsStore();
+
+
+onMounted(async () => {
+  await productStore.fetchProducts();
+});
 
 </script>
 
