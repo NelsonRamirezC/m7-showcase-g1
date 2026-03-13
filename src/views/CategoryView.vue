74d5c6ef5f8652d66206752e22501b01165391cb
Nelson <xhyskoax@gmail.com>
2026-03-13 20:39:26 -0300

    Se agrega ejemplo sencillo de breadcrumb

--- src/views/CategoryView.vue
+++ src/views/CategoryView.vue
@@ -5,7 +5,14 @@
     <main class="container py-3">
 
         <section>
-            <div class="d-flex justify-content-end">
+            <div class="d-flex justify-content-between">
+                <nav style="--bs-breadcrumb-divider: url(&#34;data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' width='8' height='8'%3E%3Cpath d='M2.5 0L1 1.5 3.5 4 1 6.5 2.5 8l4-4-4-4z' fill='%236c757d'/%3E%3C/svg%3E&#34;);"
+                    aria-label="breadcrumb">
+                    <ol class="breadcrumb">
+                        <li class="breadcrumb-item"><RouterLink to="/">Home</RouterLink></li>
+                        <li class="breadcrumb-item active" aria-current="page">Category</li>
+                    </ol>
+                </nav>
                 <button class="btn btn-primary" @click="atras">Volver atrás</button>
             </div>
 
